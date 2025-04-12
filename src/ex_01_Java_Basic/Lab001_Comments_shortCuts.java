4f549b63c557fbf4bd130b02cc102601ca5d6654
lokesh <lokeshn170915@gmail.com>
2025-04-12 12:13:19 +0530

    09-04-2024

---
+++ src/ex_01_Java_Basic/Lab001_Comments_shortCuts.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab001_Comments_shortCuts {
+    public static void main(String[] args) {
+        System.out.println("Here we are using single line comments");
+// the above line will print the single line
+        System.out.println("now we will use multi line comments");
+        /*this is
+        multi line comments
+        which will ingored by java
+         */
+
+
+    }
+}
