f1cbd2e2765241890d6f830fc47f0f8ea83baa21
lokesh <lokeshn170915@gmail.com>
2025-04-12 13:26:14 +0530

    12-04-2025

--- src/ex_01_Java_Basic/Lab002_Java_Basics.java
+++ src/ex_01_Java_Basic/Lab002_Java_Basics.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class Lab003_Java_Basics {
+public class Lab002_Java_Basics {
     public static void main(String[] args) {
         System.out.println("This is another program");
     }
