1e0aeefe5f3a46b9f58c6f453a084264fad96af4
lokesh <lokeshn170915@gmail.com>
2025-05-10 12:49:24 +0530

    11t March

---
+++ src/ex_02_Java_Basics/Lab012_Variable.java
@@ -0,0 +1,25 @@
+package ex_02_Java_Basics;
+
+public class Lab012_Variable {
+    public static void main(String[] args) {
+
+        int a=20;
+        int b=30;
+        int c;
+
+        a = a + 20;
+        b = b + 30;
+        c = a + b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        System.out.printf("%d",a);
+        System.out.printf("%d",b);
+        System.out.printf("%d",c);
+
+
+    }
+}
+
